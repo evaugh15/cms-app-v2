799167527955205ec4b191490ea625fac29b1f9a
evaugh15 <evaugh15@students.kennesaw.edu>
2022-04-06 20:40:28 -0400

    update css

--- cms/src/app/users/list-users/list-users.component.ts
+++ cms/src/app/users/list-users/list-users.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UsersService } from '../users.service';
+import { UsersService } from '../../users.service';
 
 @Component({
   selector: 'app-list-users',
