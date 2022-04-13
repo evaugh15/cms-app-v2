5f1489fac2b5fd831a0ad4bb0750a7afdeed1370
dsmit752 <98930318+dsmit752@users.noreply.github.com>
2022-04-13 18:20:07 -0400

    update webserver.js
    testing replacing 8080 with 27017

--- webserver.js
+++ webserver.js
@@ -1,7 +1,7 @@
 const http = require('https');
 const app = require('./app');
 const server = http.createServer(app);
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 27017;
 
 app.listen(port, () => {
     console.log("Server is running on port " +port);
